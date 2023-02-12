0f206966237c3e620437c252da90faf8bc93ed1e
JPBarroso <42369917+JPBarroso@users.noreply.github.com>
2023-02-12 20:02:33 +0100

    Scripts y cambios en escenas

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField]
+    Material material;
+    public Color _rojo;
+    public Color _verde;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void cambiaColor()
+    {
+        if(this.GetComponent<Slider>().value == 0)
+        {
+            material.color = _rojo;
+        }
+        else
+        {
+            material.color = _verde;
+        }
+    }
+}
