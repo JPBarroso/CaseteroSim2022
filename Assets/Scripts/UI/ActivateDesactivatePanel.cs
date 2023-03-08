3afee53356e13630e5d8b474f38eacf3d86c0afc
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-03-08 18:07:03 +0100

    Creacion de configuracion de casetas y activar desactivar paneles funcion

---
+++ Assets/Scripts/UI/ActivateDesactivatePanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDesactivatePanel : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject panel;
+    
+    private void OnEnable()
+    {
+        panel.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        panel.SetActive(true);
+    }
+}
--- Assets/Scripts/UI/ActivateUIComponent.cs
+++ Assets/Scripts/UI/ActivateUIComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,10 @@ using UnityEngine;
 public class ActivateUIComponent : MonoBehaviour
 {
     [SerializeField] private GameObject panelToActivate;
+    
 
     public void ActivateThisUIComponent()
     {
         panelToActivate.SetActive(true);
     }
-    
 }
