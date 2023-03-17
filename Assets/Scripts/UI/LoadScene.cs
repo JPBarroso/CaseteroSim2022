56d050f6a20439e06621300f9400543dd33228e3
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-03-17 18:12:04 +0100

    script para empezar partida

---
+++ Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void LoadSceneByName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    
+    
+}
