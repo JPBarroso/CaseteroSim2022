3db7230eddc581192c468975645ee203c6a4502f
JPBarroso <42369917+JPBarroso@users.noreply.github.com>
2023-03-27 21:12:03 +0200

    Haciendo menú postgame

--- Assets/Scripts/ShopSystem.cs
+++ Assets/Scripts/ShopSystem.cs
@@ -9,15 +9,23 @@ public class ShopSystem : MonoBehaviour
 {
     [SerializeField] private Shop shop;
     [SerializeField] private TMP_Text moneyText;
+    public int inicial;
 
     private void Start()
     {
         shop.InitAmountOfMoney();
         UpdateUI();
+        inicial = (int)shop.moneyAvailable;
     }
 
     public void UpdateUI()
     {
         moneyText.text = shop.moneyAvailable.ToString(CultureInfo.CurrentCulture);
+    }
+
+    public int EndMoney()
+    {
+        int final = inicial - (int)shop.moneyAvailable;
+        return final;
     }
 }
--- Assets/Scripts/UI/FinishPanel.cs
+++ Assets/Scripts/UI/FinishPanel.cs
@@ -1,27 +1,43 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinishPanel : MonoBehaviour
 {
     [Header("Referencias")]
+    [SerializeField] private Material mat;
+    [SerializeField] private Image effect;
+    [SerializeField] private ShopSystem sistema;
     [SerializeField] private TMP_InputField casetaNameInputTxt;
     [SerializeField] private TMP_Text casetaNameTxt;
     [SerializeField] private TMP_Text artTxt;
     [SerializeField] private TMP_Text luxTxt;
-    
+    [SerializeField] private TMP_Text moneyText;
+    [SerializeField] private string arteNegativo;
+    [SerializeField] private string arteNeutral;
+    [SerializeField] private string artePositivo;
+    [SerializeField] private string lujoNegativo;
+    [SerializeField] private string lujoNeutral;
+    [SerializeField] private string lujoPositivo;
+    [SerializeField] private string totalDinero;
+    [SerializeField] private string puntos;
+
+
     [Header("Objs and datas")]
     [SerializeField] private GameObject[] allCanvasObj;
     [SerializeField] private FurnitureData[] datas;
 
     private int artValue;
     private int luxValue;
+    private int moneyValue;
 
     private void OnEnable()
     {
         AllCanvasActivateOrDesactivate(false);
         SetCasetaName();
         SetArtAndLuxValues();
+        effect.color = mat.color;
     }
 
     private void OnDisable()
@@ -48,7 +64,33 @@ public class FinishPanel : MonoBehaviour
             artValue += datas[i].Data.ArtisticValue;
             luxValue += datas[i].Data.LuxuryValue;
         }
-
+        switch (artValue)
+        {
+            case < 0:
+                artTxt.text = arteNegativo + " " + MathF.Abs(artValue) + " " + puntos;
+                break;
+            case 0:
+                artTxt.text = arteNeutral;
+                break;
+            case > 0:
+                artTxt.text = artePositivo + " " + artValue + " " + puntos;
+                break;
+        }
+        switch (luxValue)
+        {
+            case < 0:
+                luxTxt.text = lujoNegativo + " " + MathF.Abs(luxValue) + " " + puntos;
+                break;
+            case 0:
+                luxTxt.text = lujoNeutral;
+                break;
+            case > 0:
+                luxTxt.text = lujoPositivo + " " + Mathf.Abs(luxValue) + " " + puntos;
+                break;
+        }
+        moneyValue = sistema.EndMoney();
+        moneyText.text = totalDinero + " " + moneyValue.ToString() + ".";
+        
         artTxt.text = "El valor de arte es" + artValue.ToString();
         luxTxt.text = "El valor de lujo es" + luxValue.ToString();
     }
