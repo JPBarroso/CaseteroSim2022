a2b362079d50b9fc3e7fcebf51d48f6c2918fc0c
JPBarroso <42369917+JPBarroso@users.noreply.github.com>
2023-04-06 05:57:55 +0200

    Rehaciendo el sistema de desafíos

--- Assets/Scripts/ChallengeSystem/ChallengeSystem.cs
+++ Assets/Scripts/ChallengeSystem/ChallengeSystem.cs
@@ -4,76 +4,36 @@ using UnityEngine;
 
 public class ChallengeSystem : MonoBehaviour
 {
-    public Challenge[] desafios;
-    public List<FurnitureData> muebles = new List<FurnitureData>(); //Hay que añadir objetos a esta lista según se compren.
+    [SerializeField] private Challenge[] desafios;
+    [SerializeField] private FurnitureData[] datas;
+    [SerializeField] private ShopSystem sistema;
+
+    private int artValue;
+    private int luxValue;
+    private int moneyValue;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        NotificationCenter.DefaultCenter().AddObserver(this, "compra");
+        
     }
-    public void compra()
+    public void Calculate()
     {
-        //Se añaden datos del mueble a la lista.
+        datas = FindObjectsOfType<FurnitureData>();
+        for (int i = 0; i < datas.Length; i++)
+        {
+            artValue += datas[i].Data.ArtisticValue;
+            luxValue += datas[i].Data.LuxuryValue;
+            moneyValue += (int)datas[i].Data.furniturePrice;
+        }
+
+    }
+    void ReadChallenge()
+    {
+
     }
     
-    bool LeerDesafioPresupuesto(Challenge desafio) //Vamos a leer lo que el jugador se ha gastado
-    {
-        float totalgastado = 0;
-        foreach(FurnitureData item in muebles) //Por cada mueble en la lista...
-        {
-            totalgastado = totalgastado + item.Data.furniturePrice; //Añadimos su valor al conteo
-        }
-        if(desafio.comparador == Challenge.Comparers.MAS) //En este caso, el jugador tiene que haberse gastado COMO MÍNIMO una cantidad
-        {
-            if(totalgastado >= desafio.valor) //Por lo que si se ha gastado esa cantidad o más...
-            {
-                return true; //Ha completado el desafío
-            }
-            else //Y si no...
-            {
-                return false; //Lo ha fallado
-            }
-        }
-        else //Ahora al revés
-        {
-            if(totalgastado < desafio.valor) //Si se ha gastado menos...
-            {
-                return true; //Ha sabido ahorrar, pasa el desafío
-            }
-            else //Y si no...
-            {
-                return false; //Haber estudiao.
-            }
-        }
-    }
-    bool LeerDesafioTipo(Challenge desafio)
-    {
-        int mesas = 0;
-        int sillas = 0;
-        int electronica = 0;
-        int bares = 0;
-        foreach(FurnitureData item in muebles)
-        {
-            if(item.Data.typeEnum == Furniture.FurnitureType.CHAIR)
-            {
-                sillas++;
-            }
-            else if(item.Data.typeEnum == Furniture.FurnitureType.TABLE)
-            {
-                mesas++;
-            }
-            else if(item.Data.typeEnum == Furniture.FurnitureType.BAR)
-            {
-                bares++;
-            }
-            else if(item.Data.typeEnum == Furniture.FurnitureType.ELECTRONICS)
-            {
-                electronica++;
-            }
-        } //Leemos la lista y sumamos por cada tipo de objeto.
-        //Ahora vendría el if/else grande.
-        return true; // por el momento
-    }
-
+    
 
 }
