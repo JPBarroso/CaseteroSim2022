22f83ebe857cd716713bb811d6ae17cdaefe5bcd
JPBarroso <42369917+JPBarroso@users.noreply.github.com>
2023-04-15 23:19:18 +0200

    Music fix

---
+++ Assets/Scripts/BGMusic.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMusic : MonoBehaviour
+{
+    public static BGMusic instance;
+    private void Awake()
+    {
+        SceneManager.activeSceneChanged += SceneDestruction;
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+
+        
+    }
+    void SceneDestruction(Scene actual, Scene previa)
+    {
+        Scene thisscene;
+        thisscene = SceneManager.GetActiveScene();
+        if(thisscene.buildIndex != 3 && thisscene.buildIndex != 6)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BGMusicTuto.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMusicTuto : MonoBehaviour
+{
+    public static BGMusicTuto instance;
+    private void Awake()
+    {
+        SceneManager.activeSceneChanged += SceneDestruction;
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+
+        
+    }
+    void SceneDestruction(Scene actual, Scene previa)
+    {
+        Scene thisscene;
+        thisscene = SceneManager.GetActiveScene();
+        if(thisscene.buildIndex != 1 && thisscene.buildIndex != 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
